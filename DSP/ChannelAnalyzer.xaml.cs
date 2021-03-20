019e5c126052a8085ba45a4d70b6a630fc860dd7
FmFromB <63696790+FmFromB@users.noreply.github.com>
2021-03-20 14:24:26 +1000

    Add files via upload

---
+++ DSP/ChannelAnalyzer.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace DSP
+{
+    /// <summary>
+    /// Логика взаимодействия для ChannelAnalyzer.xaml
+    /// </summary>
+    public partial class ChannelAnalyzer : Window
+    {
+        public ChannelAnalyzer(int channel_num, int samples_num, string[] date_channel, string channels_names)
+        {
+            InitializeComponent();
+
+            // j - канал
+            string[] date_channels = new string[channel_num + 1];
+            for (int i = 0; i < samples_num; i++)
+            {
+                string[] tmp = date_channel[i].Split(' ');
+                for (int j = 0; j < channel_num; j++)
+                {
+                    date_channels[j] += tmp[j] + ',';
+                }
+            }
+
+            string[] names = channels_names.Split(';');
+
+            for (int i = 0; i < channel_num; i++) {
+                string tm = date_channels[i];
+                string[] results = tm.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                List<double> channel_tmp = new List<double>();
+                channel_tmp.AddRange(results.Select(x => Convert.ToDouble(x)));
+                TextBlock name = new TextBlock();
+                name.Text = names[i];
+                Grid.SetRow(name, i);
+                TestGrid.Children.Add(name);
+                CartesianChart ch = new CartesianChart();
+                ch.Series = new SeriesCollection
+                {
+                    new LineSeries
+                    {
+                        LineSmoothness = 1,
+                        StrokeThickness = 2,
+                        DataLabels = false,
+                        Fill = Brushes.Transparent,
+                        PointGeometrySize = 0,
+                        Title = names[i],
+                        Values = new ChartValues<double>(channel_tmp),
+                    }
+                };
+                //ch.AxisX.MajorGrid.Enabled = false;
+                //ch.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+                RowDefinition rowDef = new RowDefinition();
+                TestGrid.RowDefinitions.Add(rowDef);
+                Grid.SetRow(ch, i);
+                TestGrid.Children.Add(ch);
+            }
+        }
+    }
+}
---
+++ DSP/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using System.Collections;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace DSP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int channel_num;
+        public int samples_num;
+        public long sampling_rate;
+        public string start_date;
+        public string start_time;
+        public string channels_names;
+        public string[] date_channel;
+        string[] data_all;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loading_file(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовый файл (*.txt)|*.txt|Звуковой файл (*.wav)|*.wav|Dat-файл (*.dat)|*.dat";
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+                System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+                data_all = File.ReadAllLines(openFileDialog.FileName);
+
+                // Важные переменные
+                channel_num = Convert.ToInt32(data_all[1]);
+                samples_num = Convert.ToInt32(data_all[3]);
+                double k = Double.Parse(data_all[5]);
+                sampling_rate = Convert.ToInt32(k);
+                start_date = data_all[7];
+                start_time = data_all[9];
+                channels_names = data_all[11];
+                string[] date_channel = new string[samples_num];
+                Array.Copy(data_all, 12, date_channel, 0, samples_num);
+
+                ChannelAnalyzer analyzer = new ChannelAnalyzer(channel_num, samples_num, date_channel, channels_names);
+                analyzer.Show();
+            }
+
+        }
+        private void signal_information(object sender, RoutedEventArgs e)
+        {
+            Signal_information signal_Information = new Signal_information();
+            signal_Information.Show();
+
+            signal_Information.chanels_num.Text = channel_num.ToString();
+            signal_Information.samples_num.Text = samples_num.ToString();
+            signal_Information.sampling_rate.Text = sampling_rate.ToString();
+            signal_Information.start.Text = start_date + " " + start_time;
+            
+            // длительность в секундах
+            var ts = TimeSpan.FromSeconds(samples_num);
+            
+            int hours = ts.Hours;
+            int minutes = ts.Minutes % 60;
+            int seconds = ts.Seconds % 60;
+            signal_Information.duration.Text = ts.Days.ToString() + "суток " +
+                                               hours.ToString() + "часов " +
+                                               minutes.ToString() + "минут " +
+                                               seconds.ToString() + "секунд";
+
+            
+            string[] date_start = start_date.Split('-');
+            string[] time_end = start_time.Split(':');
+            DateTime date_s = new DateTime(Convert.ToInt32(date_start[2]),
+                                          Convert.ToInt32(date_start[1]),
+                                          Convert.ToInt32(date_start[0]),
+                                          Convert.ToInt32(time_end[0]),
+                                          Convert.ToInt32(time_end[1]),
+                                          (int)Double.Parse(time_end[2]));
+
+            DateTime date_e = date_s + ts;
+            signal_Information.end.Text = date_e.ToString();
+        }
+    }
+}
